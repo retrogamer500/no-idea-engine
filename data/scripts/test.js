9b288327c719380b3f3030f52b58c1bc37026c68
Logan Ford <retrogamer500@gmail.com>
2019-09-04 20:24:16 -0400

    JS Scripting Changes

--- data/scripts/test.js
+++ data/scripts/test.js
@@ -1,11 +1,10 @@
 'use strict'
 function getClass() {
     return Java.extend(Java.type('net.loganford.noideaengine.scripting.engine.javascript.JsScriptEngineTest$ScriptedEntity'), {
-        i:1,
         onCreate: function(game, scene) {
             Java.superCall();
             this.setPos(500 * Math.random(), 500 * Math.random());
-            this.setSprite(game.getSpriteManager().get("test_sprite"));
+            this.setSprite(game.getSpriteManager().get('test_sprite'));
         }
     });
 }
