cc7fd94eb1e21c626dee09b27912f6196f242494
Logan Ford <retrogamer500@gmail.com>
2019-10-03 15:13:31 -0400

    Refactoring Line

--- src/main/java/net/loganford/noideaengine/shape/Line.java
+++ src/main/java/net/loganford/noideaengine/shape/Line.java
@@ -3,69 +3,67 @@ package net.loganford.noideaengine.shape;
 import lombok.Getter;
 import lombok.Setter;
 import org.joml.Vector3f;
-import org.joml.Vector3fc;
 
 public class Line extends Shape {
-    @Getter @Setter private float x1;
-    @Getter @Setter private float y1;
-    @Getter @Setter private float z1;
-    @Getter @Setter private float x2;
-    @Getter @Setter private float y2;
-    @Getter @Setter private float z2;
+    private static Vector3f V3F = new Vector3f();
 
-    private Vector3f beginning = new Vector3f();
-    private Vector3f ending = new Vector3f();
+    @Getter @Setter private Vector3f beginning = new Vector3f();
+    @Getter @Setter private Vector3f ending = new Vector3f();
 
     public Line(float x1, float y1, float x2, float y2) {
         super();
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+
+        this.beginning.set(x1, y1, 0);
+        this.ending.set(x2, y2, 0);
     }
 
-    public Line(Vector3f begin, Vector3f end) {
+    public Line(float x1, float y1, float z1, float x2, float y2, float z2) {
         super();
 
-        this.x1 = begin.x;
-        this.y1 = begin.y;
-        this.z1 = begin.z;
+        this.beginning.set(x1, y1, z1);
+        this.ending.set(x2, y2, z2);
+    }
 
-        this.x2 = end.x;
-        this.y2 = end.y;
-        this.z2 = end.z;
+    public Line(Vector3f beginning, Vector3f ending) {
+        super();
+
+        this.beginning.set(beginning);
+        this.ending.set(ending);
     }
 
     @Override
     public void setPosition(Vector3f position) {
-        float diffX = position.x - x1;
-        float diffY = position.y - y1;
-        x1 += diffX;
-        x2 += diffX;
-        y1 += diffY;
-        y2 += diffY;
-    }
-
-    public Vector3fc getBeginning() {
-        return beginning.set(x1, y1, z1);
-    }
-
-    public Vector3fc getEnding() {
-        return ending.set(x2, y2, z2);
+        Vector3f difference = V3F.set(beginning).sub(position);
+        beginning.add(difference);
+        ending.add(difference);
     }
 
     @Override
     public void getPosition(Vector3f position) {
-        position.x = x1;
-        position.y = y1;
-        position.z = z1;
+        position.set(beginning);
     }
 
     @Override
     public void getBoundingBox(Rect rect) {
-        rect.setX(Math.min(x1, x2));
-        rect.setY(Math.min(y1, y2));
-        rect.setWidth(Math.abs(x1 - x2));
-        rect.setHeight(Math.abs(y1 - y2));
+        rect.setX(Math.min(getX1(), getX2()));
+        rect.setY(Math.min(getY1(), getY2()));
+        rect.setWidth(Math.abs(getX1() - getX2()));
+        rect.setHeight(Math.abs(getY1() - getY2()));
     }
+
+    public float getX1() {return beginning.x;}
+    public float getY1() {return beginning.y;}
+    public float getZ1() {return beginning.z;}
+
+    public float getX2() {return ending.x;}
+    public float getY2() {return ending.y;}
+    public float getZ2() {return ending.z;}
+
+    public void setX1(float x1) {beginning.x = x1;}
+    public void setY1(float y1) {beginning.y = y1;}
+    public void setZ1(float z1) {beginning.z = z1;}
+
+    public void setX2(float x2) {ending.x = x2;}
+    public void setY2(float y2) {ending.y = y2;}
+    public void setZ2(float z2) {ending.z = z2;}
 }
