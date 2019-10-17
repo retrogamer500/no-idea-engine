84fe02de862cf4a9f95e2182ac264bbf28ad4903
Ford, Logan <Logan.Ford@asurion.com>
2019-10-17 10:00:17 -0400

    Improved point-face intersection handling

--- src/main/java/net/loganford/noideaengine/shape/sweepHandlers/PointFaceSweepHandler.java
+++ src/main/java/net/loganford/noideaengine/shape/sweepHandlers/PointFaceSweepHandler.java
@@ -12,50 +12,41 @@ public class PointFaceSweepHandler implements SweepHandler<Point, Face> {
     private static Vector3f V3F_1 = new Vector3f();
     private static Vector3f V3F_2 = new Vector3f();
     private static Vector3f V3F_3 = new Vector3f();
+    private static Vector3f V3F_4 = new Vector3f();
 
     @Override
     public void sweep(SweepResult result, Point point, Vector3fc velocity, Face face) {
         result.clear();
 
         //Compute normal of face
-        Vector3f edge0 = V3F.set(face.getV1()).sub(face.getV0());
-        Vector3f edge1 = V3F_1.set(face.getV2()).sub(face.getV0());
-        Vector3f normal = V3F_2.set(edge0).cross(edge1);
+        Vector3f edge1 = V3F.set(face.getV1()).sub(face.getV0());
+        Vector3f edge2 = V3F_1.set(face.getV2()).sub(face.getV0());
 
-        float normalDotVelocity = normal.dot(velocity);
-        if(Math.abs(normalDotVelocity) < MathUtils.EPSILON) {
+        Vector3f h = V3F_2.set(velocity).cross(face.getV0());
+        float det = edge1.dot(h);
+
+        if(det > -MathUtils.EPSILON && det < MathUtils.EPSILON) {
             return;
         }
 
-        float d = normal.dot(face.getV0());
-        float t = (normal.dot(point.getPosition()) + d) / normalDotVelocity;
+        float invDet = 1f / det;
+        Vector3f s = V3F_3.set(point.getPosition()).sub(face.getV0());
+        float u = invDet * (s.dot(h));
+
+        if (u < 0.0 || u > 1.0) {
+            return;
+        }
+
+        Vector3f q = V3F_4.set(s).cross(edge1);
+        float v = invDet * velocity.dot(q);
+
+        if (v < 0.0 || u + v > 1.0) {
+            return;
+        }
+
+        float t = invDet * edge2.dot(q);
 
         if(t < 0 || t > 1) {
-            return;
-        }
-
-        //Ray intersects plane
-
-        //Test if outside edge 0
-        Vector3f vp0 = V3F_3.set(point.getPosition()).sub(face.getV0());
-        Vector3f cross = V3F_1.set(edge0).cross(vp0);
-        if(normal.dot(cross) < 0) {
-            return;
-        }
-
-        //Test if outside edge 1
-        edge1 = V3F.set(face.getV2()).sub(face.getV1());
-        Vector3f vp1 = V3F_3.set(point.getPosition()).sub(face.getV1());
-        cross = V3F_1.set(edge1).cross(vp1);
-        if(normal.dot(cross) < 0) {
-            return;
-        }
-
-        //Test if outside edge 2
-        Vector3f edge2 = V3F.set(face.getV0()).sub(face.getV2());
-        Vector3f vp2 = V3F_3.set(point.getPosition()).sub(face.getV2());
-        cross = V3F_1.set(edge2).cross(vp2);
-        if(normal.dot(cross) < 0) {
             return;
         }
 
