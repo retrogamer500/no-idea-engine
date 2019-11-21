200d42b30fdac75a10b2ff4fda79a785ffc79d0b
Ford, Logan <Logan.Ford@asurion.com>
2019-11-21 13:19:21 -0500

    Implemented shader #include preprocessor directive

--- src/main/java/net/loganford/noideaengine/Property.java
+++ src/main/java/net/loganford/noideaengine/Property.java
@@ -5,8 +5,24 @@ import lombok.Setter;
 import net.loganford.noideaengine.resources.Resource;
 
 public class Property extends Resource {
-    @Getter @Setter private String stringValue;
-    @Getter @Setter private float floatValue;
-    @Getter @Setter private int intValue;
-    @Getter @Setter private boolean booleanValue;
+    @Setter private String stringValue;
+    @Getter @Setter private Float floatValue;
+    @Getter @Setter private Integer intValue;
+    @Getter @Setter private Boolean booleanValue;
+
+    public String getStringValue() {
+        if(stringValue != null) {
+            return stringValue;
+        }
+        if(floatValue != null) {
+            return floatValue.toString();
+        }
+        if(intValue != null) {
+            return intValue.toString();
+        }
+        if(booleanValue != null) {
+            return booleanValue.toString();
+        }
+        return "";
+    }
 }
