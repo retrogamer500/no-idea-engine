92b8ce2f2f21420b49ff243c9b27a560449d2f98
Ford, Logan <Logan.Ford@asurion.com>
2019-12-12 16:26:30 -0500

    Adding octree under utils

--- src/main/java/net/loganford/noideaengine/systems/collision/OctreeCollisionSystem.java
+++ src/main/java/net/loganford/noideaengine/systems/collision/OctreeCollisionSystem.java
@@ -10,6 +10,7 @@ import net.loganford.noideaengine.shape.*;
 import net.loganford.noideaengine.state.Scene;
 import net.loganford.noideaengine.utils.annotations.AnnotationUtil;
 import net.loganford.noideaengine.utils.annotations.Argument;
+import net.loganford.noideaengine.utils.collections.Octree;
 import net.loganford.noideaengine.utils.messaging.Signal;
 import org.joml.Vector3f;
 import org.joml.Vector3fc;
@@ -25,10 +26,11 @@ public class OctreeCollisionSystem extends CollisionSystem {
     private static Set SET = new HashSet<>();
     private static List LIST = new ArrayList<>();
 
+    private Octree<Shape> octree;
+
     @Getter @Setter private int maxDepth = 12;
     @Getter @Setter private int maxContents = 8;
     @Getter @Setter private float initialSize = 1024f;
-    private Node root;
 
     public OctreeCollisionSystem(Game game, Scene scene, Argument[] args) {
         super(game, scene, args);
@@ -37,25 +39,27 @@ public class OctreeCollisionSystem extends CollisionSystem {
         AnnotationUtil.getArgumentOptional("maxContents", args).ifPresent((a) -> maxContents = a.intValue());
         AnnotationUtil.getArgumentOptional("initialSize", args).ifPresent((a) -> initialSize = a.floatValue());
 
-        root = new Node(new Vector3f(), initialSize, 0);
+        octree = new Octree<>(maxDepth, maxContents, initialSize);
     }
 
     @Override
     public boolean collidesWith(Shape shape, Class<?> clazz) {
         boolean[] result = {false};
 
-        performOctreeAction(shape, (shapes) -> {
+        octree.performAction(shape, (node) -> {
+            List<Shape> shapes = node.getContents();
+
             for (int i = 0; i < shapes.size(); i++) {
                 Shape otherShape = shapes.get(i);
                 if (otherShape != shape &&
                         clazz.isAssignableFrom(otherShape.getOwningEntity().getClass()) &&
                         otherShape.collidesWith(shape)) {
                     result[0] = true;
-                    return OctreeActionResult.EXIT_EARLY;
+                    return Octree.ActionResult.EXIT_EARLY;
                 }
             }
 
-            return OctreeActionResult.CONTINUE;
+            return Octree.ActionResult.CONTINUE;
         });
 
         return result[0];
@@ -66,18 +70,20 @@ public class OctreeCollisionSystem extends CollisionSystem {
     public <C> C getCollision(Shape shape, Class<C> clazz) {
         Entity[] result = {null};
 
-        performOctreeAction(shape, (shapes) -> {
+        octree.performAction(shape, (node) -> {
+            List<Shape> shapes = node.getContents();
+
             for (int i = 0; i < shapes.size(); i++) {
                 Shape otherShape = shapes.get(i);
                 if (otherShape != shape &&
                         clazz.isAssignableFrom(otherShape.getOwningEntity().getClass()) &&
                         otherShape.collidesWith(shape)) {
                     result[0] = otherShape.getOwningEntity();
-                    return OctreeActionResult.EXIT_EARLY;
+                    return Octree.ActionResult.EXIT_EARLY;
                 }
             }
 
-            return OctreeActionResult.CONTINUE;
+            return Octree.ActionResult.CONTINUE;
         });
 
         return (C) result[0];
@@ -97,7 +103,8 @@ public class OctreeCollisionSystem extends CollisionSystem {
         SET.clear();
         Set<C> resultSet = (Set<C>) SET;
 
-        performOctreeAction(shape, (shapes) -> {
+        octree.performAction(shape, (node) -> {
+            List<Shape> shapes = node.getContents();
             for (int i = 0; i < shapes.size(); i++) {
                 Shape otherShape = shapes.get(i);
                 if (otherShape != shape &&
@@ -107,7 +114,7 @@ public class OctreeCollisionSystem extends CollisionSystem {
                 }
             }
 
-            return OctreeActionResult.CONTINUE;
+            return Octree.ActionResult.CONTINUE;
         });
 
         list.clear();
@@ -124,7 +131,8 @@ public class OctreeCollisionSystem extends CollisionSystem {
 
         Shape sweepMask = getSweepMask(shape, velocity);
 
-        performOctreeAction(sweepMask, (shapes) -> {
+        octree.performAction(sweepMask, (node) -> {
+            List<Shape> shapes = node.getContents();
             for (int i = 0; i < shapes.size(); i++) {
                 Shape otherShape = shapes.get(i);
 
@@ -145,71 +153,8 @@ public class OctreeCollisionSystem extends CollisionSystem {
                 }
             }
 
-            return OctreeActionResult.CONTINUE;
+            return Octree.ActionResult.CONTINUE;
         });
-    }
-
-
-
-    private void performOctreeAction(Shape shape, OctreeAction action) {
-        if (shape instanceof Line) {
-            Line testLine = (Line) shape;
-            performOctreeActionWithLine(testLine, action, root);
-        }
-        else {
-            Cuboid testCuboid = shape.getBoundingBox();
-            performOctreeActionWithCuboid(testCuboid, action, root);
-        }
-    }
-
-    private OctreeActionResult performOctreeActionWithLine(Line line, OctreeAction action, Node node) {
-        if(node.shape.lineCollision(line)) {
-            if(node.hasChildNodes()) {
-                for(Node child : node.children) {
-                    OctreeActionResult result = performOctreeActionWithLine(line, action, child);
-                    if(result == OctreeActionResult.EXIT_EARLY) {
-                        return OctreeActionResult.EXIT_EARLY;
-                    }
-                }
-            }
-            else {
-                return action.perform(node.contents);
-            }
-        }
-
-        return OctreeActionResult.CONTINUE;
-    }
-
-    private OctreeActionResult performOctreeActionWithCuboid(Cuboid cuboid, OctreeAction action, Node node) {
-        if(node.shape.cuboidCollision(cuboid)) {
-            if(node.hasChildNodes()) {
-                for(Node child : node.children) {
-                    OctreeActionResult result = performOctreeActionWithCuboid(cuboid, action, child);
-                    if(result == OctreeActionResult.EXIT_EARLY) {
-                        return OctreeActionResult.EXIT_EARLY;
-                    }
-                }
-            }
-            else {
-                return action.perform(node.contents);
-            }
-        }
-
-        return OctreeActionResult.CONTINUE;
-    }
-
-    private enum OctreeActionResult {
-        EXIT_EARLY,
-        CONTINUE
-    }
-
-    private interface OctreeAction {
-        /**
-         * Lambda function to perform on a node
-         * @param node the current node to perform the action with
-         * @return - OctreeActionResult.EXIT_EARLY to stop, or OctreeActionResult.CONTINUE to keep performing node actions
-         */
-        OctreeActionResult perform(List<Shape> node);
     }
 
     @Override
@@ -245,33 +190,13 @@ public class OctreeCollisionSystem extends CollisionSystem {
             if (entity.getShape() instanceof AbstractCompoundShape) {
                 for (Shape shape : (AbstractCompoundShape) entity.getShape()) {
                     shape.setOwningEntity(entity);
-                    handleShapeAddition(shape);
+                    octree.add(shape);
                 }
             } else {
                 entity.getShape().setOwningEntity(entity);
-                handleShapeAddition(entity.getShape());
+                octree.add(entity.getShape());
             }
         }
-    }
-
-    private void handleShapeAddition(Shape shape) {
-        while(! root.shape.fullyContains(shape.getBoundingBox())) {
-            Vector3f difference = V3F;
-            difference.set(shape.getPosition()).sub(root.position);
-            boolean positiveX = difference.x > 0;
-            boolean positiveY = difference.y > 0;
-            boolean positiveZ = difference.z > 0;
-            float newPosX = root.position.x + (positiveX ? 1: -1) * root.size;
-            float newPosY = root.position.y + (positiveY ? 1: -1) * root.size;
-            float newPosZ = root.position.z + (positiveZ ? 1: -1) * root.size;
-            Node newRootNode = new Node(new Vector3f(newPosX, newPosY, newPosZ), root.size * 2, root.depth - 1);
-            newRootNode.subdivide();
-            int index = (positiveX ? 4 : 0) + (positiveY ? 2 : 0) + (positiveZ ? 1 : 0);
-            newRootNode.children[index] = root;
-            root = newRootNode;
-        }
-
-        root.add(shape);
     }
 
     private void handleEntityRemoval(Entity entity) {
@@ -279,112 +204,12 @@ public class OctreeCollisionSystem extends CollisionSystem {
             if (entity.getShape() instanceof AbstractCompoundShape) {
                 for (Shape shape : (AbstractCompoundShape) entity.getShape()) {
                     shape.setOwningEntity(entity);
-                    handleShapeRemoval(shape);
+                    octree.remove(shape);
                 }
             } else {
                 entity.getShape().setOwningEntity(entity);
-                handleShapeRemoval(entity.getShape());
+                octree.remove(entity.getShape());
             }
-        }
-    }
-
-    private void handleShapeRemoval(Shape shape) {
-        root.remove(shape);
-    }
-
-    private class Node {
-        private Vector3f position;
-        private float size;
-        private Cuboid shape = new Cuboid(0, 0, 0, 0, 0, 0);
-        private int depth;
-        private Node[] children = new Node[8];
-        private ArrayList<Shape> contents = new ArrayList<>();
-
-        public Node(Vector3f position, float size, int depth) {
-            this.position = position;
-            this.size = size;
-            this.depth = depth;
-
-            shape.setPosition(new Vector3f().set(position).sub(new Vector3f(size)));
-            shape.setSize(new Vector3f().set(size * 2));
-        }
-
-        public boolean hasChildNodes() {
-            return children[0] != null;
-        }
-
-        public void subdivide() {
-            children[0] = new Node(new Vector3f(position.x + size/2, position.y + size/2, position.z + size/2), size / 2, depth + 1);
-            children[1] = new Node(new Vector3f(position.x + size/2, position.y + size/2, position.z - size/2), size / 2, depth + 1);
-            children[2] = new Node(new Vector3f(position.x + size/2, position.y - size/2, position.z + size/2), size / 2, depth + 1);
-            children[3] = new Node(new Vector3f(position.x + size/2, position.y - size/2, position.z - size/2), size / 2, depth + 1);
-            children[4] = new Node(new Vector3f(position.x - size/2, position.y + size/2, position.z + size/2), size / 2, depth + 1);
-            children[5] = new Node(new Vector3f(position.x - size/2, position.y + size/2, position.z - size/2), size / 2, depth + 1);
-            children[6] = new Node(new Vector3f(position.x - size/2, position.y - size/2, position.z + size/2), size / 2, depth + 1);
-            children[7] = new Node(new Vector3f(position.x - size/2, position.y - size/2, position.z - size/2), size / 2, depth + 1);
-        }
-
-        public void add(Shape shape) {
-            if(!hasChildNodes()) {
-                if(contents.size() + 1 > maxContents && depth < maxDepth) {
-                    subdivide();
-
-                    for(Shape existingShape : contents) {
-                        addShapeToChildrenIfAble(existingShape);
-                    }
-                    contents.clear();
-                    addShapeToChildrenIfAble(shape);
-                }
-                else {
-                    contents.add(shape);
-                }
-            }
-            else if(hasChildNodes()) {
-                addShapeToChildrenIfAble(shape);
-            }
-        }
-
-        public void remove(Shape shape) {
-            if(!hasChildNodes()) {
-                contents.remove(shape);
-            }
-            else {
-                boolean childrenEmpty = true;
-                for(Node child : children) {
-                    if(child.shape.cuboidCollision(shape.getBoundingBox())) {
-                        child.remove(shape);
-                    }
-                    if(child.contents.size() > 0 || child.hasChildNodes()) {
-                        childrenEmpty = false;
-                    }
-                }
-                if(childrenEmpty) {
-                    for(int i = 0; i < children.length; i++) {
-                        children[i] = null;
-                    }
-                }
-            }
-        }
-
-        public void addShapeToChildrenIfAble(Shape shape) {
-            for(Node child : children) {
-                if(child.shape.cuboidCollision(shape.getBoundingBox())) {
-                    child.add(shape);
-                }
-            }
-        }
-
-        public int countAllContents() {
-            int result = 0;
-            if(hasChildNodes()) {
-                for(Node child : children) {
-                    result += child.countAllContents();
-                }
-            }
-            else {
-                result += contents.size();
-            }
-            return result;
         }
     }
 }
